79214a24e29ab1b213de1a5cbe1417dd77953486
Ba1tan <231620@astanait.edu.kz>
2025-01-27 23:31:15 -0800

    Some needed struct files

---
+++ IKIGAI/Services/DataSavingService.swift
@@ -0,0 +1,10 @@
+//
+//  DataSavingService.swift
+//  IKIGAI
+//
+//  Created by Nursultan Bukenbayev on 27.01.2025.
+//
+
+import Foundation
+
+//Need to be implemented
---
+++ IKIGAI/ViewModels/KanbanBoardViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  KanbanBoardViewModel.swift
+//  IKIGAI
+//
+//  Created by Nursultan Bukenbayev on 27.01.2025.
+//
+
+import Foundation
+
+//NeedTo Be implemnetd
